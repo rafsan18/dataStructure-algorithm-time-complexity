3b2a665e341844c159993e15e045bb0f8888aa2c
rafsan <rafsanamin18@gmail.com>
2020-10-18 19:05:44 +0600

    stack own

--- stack.js
+++ stack.js
@@ -2,9 +2,11 @@ class Stack {
     constructor() {
         this.stack = [];
     }
+
     add(item) {
         this.stack.push(item);
     }
+
     remove() {
         if (this.stack.length) {
             return this.stack.pop();
@@ -12,7 +14,26 @@ class Stack {
     }
 }
 
+const number = new Stack();
+number.add(44);
+number.add(36);
+number.add(10);
+number.add(38);
+number.add(93);
+number.add(74);
+
+console.log(number);
+
+const removedNumber = number.remove();
+console.log({ removedNumber });
+console.log(number);
+
 const guest = new Stack();
 guest.add("Rahman");
 guest.add("Zaman");
+guest.add("Kaman");
+guest.add("Kamran");
+console.log(guest.stack);
+const speaker = guest.remove();
+console.log(speaker);
 console.log(guest.stack);
